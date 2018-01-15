0e0534fd5c2b3389e33c1e709663c02f97face2c
Luc Belliveau <luc.belliveau@nrc-cnrc.gc.ca>
2018-01-15 11:44:57 -0400

    added deploy files

---
+++ packages/reference-implementation/kubernetes/deploy-local.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+DOCKER_REPO="localhost:5000"
+DOCKER_PACKAGE="gctools-outilsgc/reference-implementation"
+
+read -p "Increment the minor revision and deploy to kubernetes? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  CURRENT=`cat $DIR/version.txt`
+  VERSION=`cat $DIR/version.txt | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+  docker rmi $DOCKER_REPO/$DOCKER_PACKAGE:$CURRENT
+  (cd $DIR/../ && yarn build && yarn docker-build-local)
+  CMD=$(echo sed -i "'s/localhost:5000\/gctools-outilsgc\/reference-implementation:$CURRENT/localhost:5000\/gctools-outilsgc\/reference-implementation:$VERSION/g'" $DIR/1_deployment.yaml)
+  eval $CMD
+  docker tag $DOCKER_PACKAGE:latest $DOCKER_REPO/$DOCKER_PACKAGE:$VERSION
+  until docker push $DOCKER_REPO/$DOCKER_PACKAGE:$VERSION
+  do
+    echo "Error pushing to server, trying again in 1 second."
+    sleep 1
+  done
+  kubectl apply -f $DIR/1_deployment.yaml
+  echo $VERSION > $DIR/version.txt
+  echo "$CURRENT ==> $VERSION deployed.";
+  exit 0
+fi
+
+echo
+echo "Fine, be that way."
+echo
