751cd89404d477d227f1be9da2047e72346bf071
steph4104 <stephanie4104@gmail.com>
2019-05-16 12:45:14 -0500

    put search into a form

--- packages/global-nav/components/Search.js
+++ packages/global-nav/components/Search.js
@@ -1,51 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button } from "reactstrap";
-
+import useForm from "react-hook-form";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Search = (props) => {
+  
   const {
     currentApp,
     currentLang,
   } = props;
 
   const searchAlt = (currentLang === 'en_CA' ? 'Search' : 'Chercher');
+  const [searchkey, setSearch] = useState("");
 
-  function SearchKey(){
-    console.log('search')
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(searchkey)
   }
 
   return (
       <div className="searchContainer">
+       <form onSubmit={handleSubmit}>
         <label htmlFor="search" className="sr-only">
           {searchAlt}
         </label>
         <Input
           className="searchBox"
-          type="search"
+          type="text"
           name="search"
           id="search"
           placeholder={searchAlt}
+          onChange={e => setSearch(e.target.value)}
+          value={searchkey}
         />
-        <Button className="btn-search" type="button" onClick={() => SearchKey(this.handleClick)}>
+        <Button className="btn-search" type="submit">
           <FontAwesomeIcon icon={faSearch} />
           <span className="sr-only">{searchAlt}</span>
         </Button>
+       </form>
       </div>
   );
 };
+
 Search.defaultProps = {
   currentApp: {
     name: 'AppName',
     id: '1',
   },
-  SearchKey: () => {},
+  setSearch: () => {},
   currentLang: 'en_CA',
   onResultClick: () => {},
 };
-
 
 Search.propTypes = {
   /** Current app object name, ID, home link and logo */
@@ -58,7 +65,7 @@ Search.propTypes = {
   /** Gets the value of the option clicked */
   onResultClick: PropTypes.func,
 
-  SearchKey: PropTypes.func
+  setSearch: PropTypes.func
 };
 
 export default Search;
