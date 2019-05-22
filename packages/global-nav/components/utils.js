78c15de599a9d8fc76dcda453002195ba87e7af2
piet0024 <n.pietrantonio0024@live.com>
2019-05-22 11:20:17 -0400

    fix scroll error on desktop

--- packages/global-nav/components/utils.js
+++ packages/global-nav/components/utils.js
@@ -1,12 +1,14 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("mobileFip").style.top = "0";
-    document.getElementById("mobileNavBar").style.top = "48px";
-  } else {
-    document.getElementById("mobileFip").style.top = "-50px";
-    document.getElementById("mobileNavBar").style.top = "0";
+  if(document.getElementById("mobileFip")) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("mobileFip").style.top = "0";
+      document.getElementById("mobileNavBar").style.top = "48px";
+    } else {
+      document.getElementById("mobileFip").style.top = "-50px";
+      document.getElementById("mobileNavBar").style.top = "0";
+    }
   }
   prevScrollpos = currentScrollPos;
 }
